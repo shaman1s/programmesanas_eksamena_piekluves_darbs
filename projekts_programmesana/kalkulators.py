5bfe21d51f0f8df818e4b2ca6fd4d2df393dc4e3
shaman1s <113180319+shaman1s@users.noreply.github.com>
2023-03-22 01:01:10 +0200

    Update kalkulators.py

--- projekts_programmesana/kalkulators.py
+++ projekts_programmesana/kalkulators.py
@@ -5,7 +5,7 @@ import uuid
 
 conn = sqlite3.connect('vesture.db') 
 c = conn.cursor()
-
+# Datubāzes izveide, ja tādas nav 
 c.execute('''
           CREATE TABLE IF NOT EXISTS pirma_formula
           ([ieraksta_id] TEXT, [E] TEXT, [F] TEXT, [S] TEXT, [REZ] TEXT)
@@ -18,23 +18,24 @@ c.execute('''
                      
 conn.commit()
 
-# Syntax to create a Window
+# Galvenā loga izveide
 window = Tk()
-window.title('Fizikas apgaismojuma formulu kalkulators') # To give the title of the window.
-window.geometry("400x470") # To initialize the size of the window. 
-window.resizable(False, False) # If you don't want to make your window resizable initialize this line. 
+window.title('Fizikas apgaismojuma formulu kalkulators') # Nosaukuma piešķiršana galvenajam logam.
+window.geometry("400x470") # Izmēru iestatīšana logam. 
+window.resizable(False, False) 
 
-num1 = IntVar() # initialize the first variable
-num2 = IntVar() # initialize the second variable
+# mainīgo deklarēšana
+num1 = IntVar() 
+num2 = IntVar() 
 num3 = IntVar()
 num4 = IntVar()
 num5 = IntVar()
 num6 = IntVar()
 
+# Tekstuālās informācijas pievienošana grafiskajam logam
 mainTitle = Label(window, text="Rēķinot nezināmo, neatstājiet tukšus lielumus!", font=("Courier", 10))
-mainTitle.place(x=10, y=10) # Use the place layout to place further labels
+mainTitle.place(x=10, y=10) 
 
-# Text
 type1 = Label(window, text="E - Apgaismojums", font=("Courier", 10))
 type1.place(x=10, y=30)
 
@@ -74,7 +75,7 @@ paskaidrojums5.place(x=10, y=450)
 
 
 
- # Use functions to give action to the button. 
+ # funkcijas nezināmā aprēķināšanai, datu izvadei un ierakstīšanai datubāzē 
 def pirma_formula():
     if num1.get()==0 and num2.get()!=0 and num3.get()!=0:
         rezultats = num2.get() / num3.get()
@@ -83,18 +84,18 @@ def pirma_formula():
     elif num1.get()!=0 and num2.get()!=0 and num3.get()==0:
         rezultats = num2.get() / num1.get()
     else:
-        rezultats = "nepareizi ievaddati"
-    ieraksta_id=str(uuid.uuid4())
-    E = str(num1.get())
+        rezultats = "nepareizi ievaddati" # Šis if-elif-else bloks pārbauda, kurš lielums jāaprēķina
+    ieraksta_id=str(uuid.uuid4()) # identifikatora piešķiršana jaunam ierakstam datubāzē
+    E = str(num1.get()) # Vērtību pielāgošana datubāzes prasītajam formātam
     F = str(num2.get())
     S = str(num3.get())
     REZ = str(rezultats)
     sql = '''INSERT INTO pirma_formula 
     VALUES(?, ?, ?, ?, ?) '''
-    c.execute(sql, (ieraksta_id, E, F, S, REZ))
+    c.execute(sql, (ieraksta_id, E, F, S, REZ)) # vērtību ierakstīšana datubāzē
     conn.commit()
     resultEntry.delete(0, END)
-    resultEntry.insert(0, str(rezultats))
+    resultEntry.insert(0, str(rezultats)) # rezultāta ievietošana grafiskajā logā
 
 def otra_formula():
     if num4.get()==0 and num5.get()!=0 and num6.get()!=0:
@@ -117,18 +118,18 @@ def otra_formula():
     resultEntry1.delete(0, END)
     resultEntry1.insert(0, str(rezultats))
 
-def vestures_tabula():
-    newWindow = Toplevel(window)
+def vestures_tabula(): # jauna grafiska loga izveide un datu izvads
+    newWindow = Toplevel(window) # jauna loga definēšana
     newWindow.title("Vēsture pirmajai formulai")
     newWindow.geometry("400x400")
-    res = c.execute("SELECT * FROM pirma_formula")
+    res = c.execute("SELECT * FROM pirma_formula") # datubāzes datu izlasīšana
     vesture = res.fetchall()
     paskaidrojums_v = Label(newWindow, text="Ar komatiem atdalīti E, Φ, S un rezultāts", font=("Courier", 10))
     paskaidrojums_v.place(x=10, y=10)
     paskaidrojums_v2 = Label(newWindow, text="Ar 0 norādīts rēķināmais lielums", font=("Courier", 10))
     paskaidrojums_v2.place(x=10, y=30)
-    for i in range (len(vesture)-1,-1,-1):
-        vertiba1 = Label(newWindow, text=f"{vesture[i][1]},{vesture[i][2]},{vesture[i][3]},{vesture[i][4]}", font=("Courier", 10))
+    for i in range (len(vesture)-1,-1,-1): # datu izvade grafiskajā logā
+        vertiba1 = Label(newWindow, text=f"{vesture[i][1]} , {vesture[i][2]} , {vesture[i][3]} , {vesture[i][4]}", font=("Courier", 10))
         vertiba1.place(x=10, y=((len(vesture)-i)+2)*20)
 
 def vestures_tabula2():
@@ -143,13 +144,13 @@ def vestures_tabula2():
     paskaidrojums_v4 = Label(newWindow2, text="Ar 0 norādīts rēķināmais lielums", font=("Courier", 10))
     paskaidrojums_v4.place(x=10, y=30)
     for i in range (len(vesture)-1,-1,-1):
-        vertiba1 = Label(newWindow2, text=f"{vesture[i][1]},{vesture[i][2]},{vesture[i][3]},{vesture[i][4]}", font=("Courier", 10))
+        vertiba1 = Label(newWindow2, text=f"{vesture[i][1]} , {vesture[i][2]} , {vesture[i][3]} , {vesture[i][4]}", font=("Courier", 10))
         vertiba1.place(x=10, y=((len(vesture)-i)+2)*20)
         
 
     
 
-# Entries Syntax
+# Ievadu sintakse
 entry1 = Entry(window, textvariable=num1)
 entry1.place(x=200, y=30, height=20, width=143)
 
@@ -168,13 +169,13 @@ entry5.place(x=200, y=160, height=20, width=143)
 entry6 = Entry(window, textvariable=num6)
 entry6.place(x=200, y=180, height=20, width=143)
 
-resultEntry = Entry(window, bg = "green")
+resultEntry = Entry(window, bg = "orange")
 resultEntry.place(x=200, y=110, height=20, width=143)
 
-resultEntry1 = Entry(window, bg = "green")
+resultEntry1 = Entry(window, bg = "orange")
 resultEntry1.place(x=200, y=220, height=20, width=143)
 
-# Buttons syntax
+# Pogu sintakse
 button_1f = Button(window, text="Aprēķināt nezināmo", fg="black", font=("Courier", 10), command=pirma_formula) # Use command to call the functions.
 button_1f.place(x=10, y=90, height=20, width=150)
 
@@ -189,4 +190,4 @@ button_vest2.place(x=10, y=290, height=20, width=200)
 
 
 
-window.mainloop()
+window.mainloop() # Cikls, kas uztur grafisko logu
